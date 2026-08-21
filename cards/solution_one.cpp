398e4ad7093da5cbd02e406d2ebea5cdfff0fe79
softboxdev <soft.box.development@gmail.com>
2026-08-21 15:16:12 +0300

    cards solution cpp

---
+++ cards/solution_one.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+long getCardCount(int n, int k, const vector<long long> &cards) {
+    int sum = 0;
+    for (int i = 0; i < k; ++i) {
+        sum += cards[i];
+        
+    }
+    int ans = sum;
+    for (int i = 0; i < k; ++i) {
+        sum += cards[n-1-i]-cards[k-1-i];
+        if(sum > ans)
+            ans = max(ans, sum);
+    }
+    return ans;
+}
+
+int readInt() {
+    int x;
+    cin >> x;
+    return x;
+}
+
+vector<long long> readList(int n) {
+    vector<long long> res(n);
+    for (int i = 0; i < n; i++) {
+        cin >> res[i];
+    }
+    return res;
+}
+
+int main() {
+    int n = readInt();
+    int k = readInt();
+    vector<long long> cards = readList(n);
+    cout << getCardCount(n, k, cards);
+}
